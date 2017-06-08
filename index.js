049a22b87fd3b9c82dad6336d031ae4fded03f8b
Rony Vidaur <rony.vidaur@gmail.com>
2017-06-07 23:47:33 -0600

    basic connection

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-var express = require('express')
+const express = require('express')
+const socket = require('socket.io')
+
+const app = express()
+const server = app.listen(3000, () => {
+   console.log('running on port 3000')
+})
+
+app.use(express.static('public'))
+
+var io = socket(server)
+
+io.on('connection', socket => {
+  console.log('made socket connection')
+})
